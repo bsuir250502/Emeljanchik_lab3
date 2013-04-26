d93de959c115872136df4fc16de908f9d936c14f
Gi5d <gi5d@yandex.ru>
2013-04-26 20:41:48 +0300

    working version 1.0
    The next version will be added to the output on the screen and view the contents!
                                                                 Gi5d

--- code.c
+++ code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define n_max_number 5
-#define plate_number 10
+#define PLATE_NUMBER 10
 struct car_list {
-  char number_plate[plate_number];
+	char number_plate[PLATE_NUMBER];
 	struct car_list *prev;
 };
 struct city_parking {
@@ -13,28 +13,30 @@ struct city_parking {
 	struct car_list *waiting_car;
 	struct city_parking *prev;
 };
-car_list free_list (car_list *must_be_free1, int number_of_parking1) {
-	car_list *required;
+struct car_list *free_list (car_list *must_be_free1, int number_on_parking1) {
+	car_list *required3;
 	int i=0, j;
-	required=must_be_free1;
-	while (required->prev!=NULL) {
-		i++;
-		required=required->prev;
-	}
-	free (required);
-	while (i>0) {
-		required=must_be_free1;
-		j=1;
-		while (j<i) {
-			j++;
-			required=required->prev;
+	if (must_be_free1!=NULL) {
+		required3=must_be_free1;
+		while (required3->prev!=NULL) {
+			i++;
+			required3=required3->prev;
 		}
-		free (required);
-		i--;
+		free (required3);
+		while (i>1) {
+			required3=must_be_free1;
+			j=1;
+			while (j<i) {
+				j++;
+				required3=required3->prev;
+			}
+			free (required3);
+			i--;
+		}
 	}
-	return *must_be_free1;
+	return must_be_free1;
 }
-city_parking free_all (city_parking *must_be_free2, int number_of_parking2) {
+struct city_parking free_all (city_parking *must_be_free2, int number_of_parking2) {
 	city_parking *required;
 	int i=0, j;
 	required=must_be_free2;
@@ -59,7 +61,7 @@ city_parking free_all (city_parking *must_be_free2, int number_of_parking2) {
 	free (required);
 	return *must_be_free2;
 }
-city_parking add_car (int number_of_parking3, city_parking *main_city_parking3) {
+struct city_parking add_car (int number_of_parking3, city_parking *main_city_parking3) {
 	city_parking *required;
 	car_list *required1;
 	int j=1;
@@ -76,7 +78,9 @@ city_parking add_car (int number_of_parking3, city_parking *main_city_parking3) 
 			return *main_city_parking3;
 		}
 		printf ("\nPlease enter plate number: ");
-		fgets (required->standing_car->number_plate, plate_number, stdin);
+		fgets (required->standing_car->number_plate, PLATE_NUMBER, stdin);
+		required->standing_car->prev=NULL;
+		return *main_city_parking3;
 	}
 	else {
 		if (required->number_of_free_places==0) {
@@ -88,7 +92,8 @@ city_parking add_car (int number_of_parking3, city_parking *main_city_parking3) 
 					return *main_city_parking3;
 				}
 				printf ("\nPlease enter plate number: ");
-				fgets (required->waiting_car->number_plate, plate_number, stdin);
+				fgets (required->waiting_car->number_plate, PLATE_NUMBER, stdin);
+				required->waiting_car->prev=NULL;
 			}
 			else {
 				required1=required->waiting_car;
@@ -101,26 +106,30 @@ city_parking add_car (int number_of_parking3, city_parking *main_city_parking3) 
 					return *main_city_parking3;
 				}
 				printf ("\nPlease enter plate number: ");
-				fgets (required1->prev->number_plate, plate_number, stdin);
+				fgets (required1->prev->number_plate, PLATE_NUMBER, stdin);
+				required1->prev->prev=NULL;
 			}
 			return *main_city_parking3;
 		}
-		required->number_of_free_places--;
-		required1=required->standing_car;
-		while (required1->prev!=NULL)
-			required1=required1->prev;
-		if (!(required1->prev=(car_list *)malloc(sizeof(car_list)))) {
-			free_all (main_city_parking3, number_of_parking3);
-			printf ("\nFatality error:\nProgram will close...");		
-			number_of_parking3=0;
+		else {
+			required->number_of_free_places--;
+			required1=required->standing_car;
+			while (required1->prev!=NULL)
+				required1=required1->prev;
+			if (!(required1->prev=(car_list *)malloc(sizeof(car_list)))) {
+				free_all (main_city_parking3, number_of_parking3);
+				printf ("\nFatality error:\nProgram will close...");		
+				number_of_parking3=0;
+				return *main_city_parking3;
+			}
+			printf ("\nPlease enter plate number: ");
+			fgets (required1->prev->number_plate, PLATE_NUMBER, stdin);
+			required1->prev->prev=NULL;
 			return *main_city_parking3;
 		}
-		printf ("\nPlease enter plate number: ");
-		fgets (required1->prev->number_plate, plate_number, stdin);
 	}
-	return *main_city_parking3;
 }
-city_parking del_car (int number_of_parking4, city_parking *main_city_parking4) {
+struct city_parking del_car (int number_of_parking4, city_parking *main_city_parking4) {
 	city_parking *required;
 	car_list *required1;
 	required=main_city_parking4;
@@ -134,7 +143,7 @@ city_parking del_car (int number_of_parking4, city_parking *main_city_parking4) 
 		return *main_city_parking4;
 	}
 	if (required->waiting_car!=NULL) {
-		for (j=0; j<plate_number; j++) {
+		for (j=0; j<PLATE_NUMBER; j++) {
 			required->standing_car->number_plate[j]=required->waiting_car->number_plate[j];
 			if (required->waiting_car->number_plate[j]=='\0')
 				break;
@@ -156,13 +165,13 @@ city_parking del_car (int number_of_parking4, city_parking *main_city_parking4) 
 	required->standing_car=required1;
 	return *main_city_parking4;
 }
-city_parking enter_information_about_parking (city_parking *main_city_parking5, int number_of_parking5) {
+struct city_parking enter_information_about_parking (city_parking *main_city_parking5, int number_of_parking5) {
 	char char_n[n_max_number];
 	int n;
 	city_parking *required;
 	required=main_city_parking5;
-	for (n=1; n<number_of_parking5; n++)		//while (required->prev!=NULL)
-		required=required->prev;				//если заполнять только последний (создал и заполнил)
+	while (required->prev!=NULL)
+		required=required->prev;
 	while (1) {
 		printf ("Please enter information about car parking:\nPlease enter total number of places on parking: ");
 		fgets (char_n, n_max_number, stdin);
@@ -180,16 +189,15 @@ city_parking enter_information_about_parking (city_parking *main_city_parking5, 
 			printf ("\nError:\nBad number, please try again...");
 		else break;
 	}
-	n=1;
-	while (n<=required->total_number_of_places-required->number_of_free_places) {
-		add_car (number_of_parking5, main_city_parking5);
+	n=required->total_number_of_places-required->number_of_free_places;
+	while (n-->0) {
 		if (number_of_parking5==0)
 			break;
-		n++;
+		add_car (number_of_parking5, main_city_parking5);
 	}
 	return *main_city_parking5;
 }
-city_parking add_city_parking (city_parking *main_city_parking6, int number_of_parking6) {
+struct city_parking add_city_parking (city_parking *main_city_parking6, int number_of_parking6) {
 	city_parking *required;
 	required=main_city_parking6;
 	while (required->prev!=NULL)
@@ -235,7 +243,6 @@ int main() {
 			}
 			add_car (number_of_parking2, city_parking_pointer);
 			if (number_of_parking==0) {
-				free (char_n);
 				return 0;
 			}
 		}
@@ -248,14 +255,13 @@ int main() {
 					printf ("\nError:\nBad number, please try again...");
 				else break;
 			}
-			del_car (number_of_parking, city_parking_pointer);
+			del_car (number_of_parking2, city_parking_pointer);
 		}
 		else if (char_n[0]=='a') {
 			add_city_parking (city_parking_pointer, number_of_parking);
 			number_of_parking++;
 			if (number_of_parking==0) {
 				printf ("\nFatality error:\nProgram will close...");
-				free (char_n);
 				return 0;
 			}
 		}
@@ -263,7 +269,5 @@ int main() {
 			break;
 		else printf ("\nError:\nBad command, please try again...");
 	}
-	//сюда можно вставить какой-нибуль вывод
 	free_all (city_parking_pointer, number_of_parking);
-	free (char_n);
 }
